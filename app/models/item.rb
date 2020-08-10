18babf2f384f3c760d1d7ef5acd426d014e12803
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-10 18:17:54 +0900

    追加：アソシエーションとバリデーション
    itemsテーブルとimagesテーブルにおけるアソシエーションを記述した。また、itemsテーブルにはバリデーションも記述した。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,20 @@
 class Item < ApplicationRecord
+  has_many :images, dependent: :destroy
+  belongs_to :category
+  accepts_nested_attributes_for :images, allow_destroy: true
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :explain, presence: true
+  validates :item_status_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :postage_id, presence: true
+  validates :shipping_date_id, presence: true
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_date
+  belongs_to_active_hash :item_status
+  belongs_to_active_hash :postage
 end
