2f7c81d3d1cf3781c3ccf50112dc4597b504d03b
kayahikomakita <kmakitatech@gmail.com>
2020-08-10 17:52:01 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  # root 'items#index'
+  root 'items#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :items
 end
