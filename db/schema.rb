d4ccdd3f166616d2b7c858f1a01f5694df706f7d
ryokumagai <rk1996kumagai@gmail.com>
2020-08-10 16:59:44 +0900

    修正: usersテーブルのカラム名
    usersテーブルのnameカラムをnicknameカラムに修正した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_075238) do
+ActiveRecord::Schema.define(version: 2020_08_10_075643) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
