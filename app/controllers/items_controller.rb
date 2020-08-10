2d86ee3a3bbe34aa6129bf2f522524b29fb2bf23
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-10 21:37:25 +0900

    追加：set_item
    現段階でitemテーブルに保存する内容を、permitメソッドを用いて定義した。
    
    親カテゴリーを取得する要素は単数なので、nameを単数形に変更した。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,9 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  private
+  def set_item
+    params.require(:item).permit(:name, :price, :explain, :size, :prefecture_id, :brand, :shipping_date_id, :item_status_id, :postage_id, :category_id)
+  end
 end
