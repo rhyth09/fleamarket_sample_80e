af10359575677824b914120a075757efb1efcf09
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-10 17:51:24 +0900

    追加：itemテーブルにおけるカラムの作成
    categoriesテーブルに必要となる10項目のカラムを、マイグレーションファイルに追記した。
    
    なお、ユーザー関連のカラムは未作成である。

--- db/migrate/20200810083844_create_items.rb
+++ db/migrate/20200810083844_create_items.rb
@@ -1,7 +1,17 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name, null: false
+      t.integer :price, null: false
+      t.text :explain, null: false
+      t.string :size
+      t.text :brand
+      t.references :category, foreign_key: true
+      
+      t.integer :prefecture_id, null: false
+      t.integer :shipping_date_id, null: false
+      t.integer :item_status_id, null: false
+      t.integer :postage_id, null: false
       t.timestamps
     end
   end
