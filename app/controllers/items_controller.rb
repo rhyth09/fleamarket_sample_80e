3096da96d1ff1513a56c2132db0baff098a5f825
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-11 09:34:18 +0900

    追加：単一画像投稿機能
    複数画像投稿機能を見据えた、単一画像投稿機能の実装を行った。fields_forヘルパーを使用した。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,17 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.includes(:images)
   end
 
   def new
     @item = Item.new
+    @item.images.build
   end
 
   def create
     @item = Item.new(set_item)
     if @item.save
-      redirect_to new_items_path, notice: "作成しました"
+      redirect_to root_path
     else
       render :new
     end
@@ -17,6 +19,6 @@ class ItemsController < ApplicationController
 
   private
   def set_item
-    params.require(:item).permit(:name, :price, :explain, :size, :prefecture_id, :brand, :shipping_date_id, :item_status_id, :postage_id, :category_id)
+    params.require(:item).permit(:name, :price, :explain, :size, :prefecture_id, :brand, :shipping_date_id, :item_status_id, :postage_id, :category_id, images_attributes: [:src])
   end
 end
