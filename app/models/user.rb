95a374af7df39f651fef6124e74e9cf8934ae384
ryokumagai <rk1996kumagai@gmail.com>
2020-08-13 00:12:34 +0900

    修正：usersテーブル
    profileテーブルのカラムをusersテーブルに統合した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name ,presence: true
+  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_year, :birth_month, :birth_day ,presence: true
 end
