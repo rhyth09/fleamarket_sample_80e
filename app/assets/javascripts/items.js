8a93f9c2cba44bf8d5b3ab10e2a2cd3b441c7a3b
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-12 16:26:54 +0900

    追加：画像入力ボタン
    画像入力ボタンを使ってファイルを選択するごとに、新しい画像入力ボタンを追加するための記述をした。

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,3 +1,15 @@
 $(document).on('turbolinks:load', ()=> {
-  
+  const buildFileField = (index) => {
+    const html = `<div class="js-file_group" data-index="${index}">
+                    <input class="js-file" type="file" name="item[images_attributes][${index}][src]" id="item_images_attributes_${index}_src">
+                  </div>`;
+    return html;
+  }
+
+  let fileIndex = [1, 2, 3, 4];
+
+  $('#image-box').on('change', function(e) {
+    $('#image-box').append(buildFileField(fileIndex[0]));
+    fileIndex.shift();    
+  });
 });
