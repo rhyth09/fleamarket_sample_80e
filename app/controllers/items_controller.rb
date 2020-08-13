73d9f2bf18f1e79ff6ed6af366ce8bbe4978c2a0
n0808 <life.with.chums.119@gmail.com>
2020-08-13 18:44:51 +0900

    追加：json.jbuilderファイル作成
    子カテゴリーと孫カテゴリーが選択できるように作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,23 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @item.images.build
+    @category_parent_array = ["---"]
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
   end
+  
+  def get_category_children
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+    # 子カテゴリーが選択された後に動くアクション
+  def get_category_grandchildren
+    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+
 
   def create
     @item = Item.new(item_params)
