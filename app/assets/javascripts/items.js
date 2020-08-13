00bf3132f39774dd5aec33adadfcc6c798d0ae86
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-13 09:39:05 +0900

    追加：プレビューの削除
    一度アップロードした画像を削除できるように、削除ボタンおよびプレビューの削除機能を実装した。

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,4 +1,6 @@
 $(document).on('turbolinks:load', ()=> {
+
+  // 追加する<input type="file">
   const buildFileField = (index) => {
     const html = `<div class="js-file_group" data-index="${index}">
                     <input class="js-file" type="file" name="item[images_attributes][${index}][src]" id="item_images_attributes_${index}_src">
@@ -6,11 +8,12 @@ $(document).on('turbolinks:load', ()=> {
     return html;
   }
 
+  // 追加するプレビュー
   const buildImage = (index, url) => {
     const html = `<div class="preview">
-    <img data-index="${index}" src="${url}" width="120px" height="120px">
-    <a href=''>編集</a>
-    </div>`;
+                    <img data-index="${index}" src="${url}" width="120px" height="120px">
+                    <div class="img-dele-btn" id="delete_btn_${index}">削除</div>
+                  </div>`;
     return html;
   }
 
@@ -21,17 +24,31 @@ $(document).on('turbolinks:load', ()=> {
     const targetIndex = $(this).parent().data('index');
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
+
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
     } else {
       $('.dropbox__img').before(buildImage(targetIndex, blobUrl));
       count ++;
       $('.dropbox__img').attr('for', `item_images_attributes_${count}_src`);
-      if (count === 5) {
+      if ($('.preview').length === 5) {
         $('label').hide();
       }
     }
+    // fileIndexの削除と追加
     $('#image-box').append(buildFileField(fileIndex[0]));
-    fileIndex.shift();    
+    fileIndex.shift();
+    fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+  });
+
+  // プレビューの削除
+  $(document).on('click', '.img-dele-btn', function() {
+    $(this).parent().remove();
+    count --;
+    if ($('.preview').length < 5) {
+      $('#image-box').append(buildFileField(fileIndex[0]));
+      $('label').show();
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+    }  
   });
 });
