2302b7dbefa09ce8351b16167eca43f6ba2d2b8b
ryokumagai <rk1996kumagai@gmail.com>
2020-08-13 12:06:52 +0900

    追加：新規登録情報保存機能
    新規登録情報（ユーザー情報、商品送付先情報）をテーブルに保存するアクションを追加した。新規登録完了後はルートパスに遷移するように設定した。

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -22,6 +22,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
     render :new_address
   end
 
+  def create_address
+    @user = User.new(session["devise.regist_data"]["user"])
+    @address = Address.new(address_params)
+    unless @address.valid?
+      flash.now[:alert] = @address.errors.full_messages
+      render :new_address and return
+    end
+    @user.build_address(@address.attributes)
+    @user.save
+    session["devise.regist_data"]["user"].clear
+    sign_in(:user, @user)
+    redirect_to root_path
+  end
+
   # GET /resource/edit
   # def edit
   #   super
@@ -46,7 +60,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def address_params
+    params.require(:address).permit(:send_first_name, :send_last_name, :send_first_name_kana, :send_last_name_kana, :postal_code, :prefecture, :city, :address, :build_name, :phone_number)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
-  validates :send_first_name, :send_last_name, :send_first_name_kana, :send_last_name_kana, :postal_code, :prefecture, :city, :address, :build_name, :phone_number ,presence: true
+  validates :send_first_name, :send_last_name, :send_first_name_kana, :send_last_name_kana, :postal_code, :prefecture, :city, :address ,presence: true
 end
