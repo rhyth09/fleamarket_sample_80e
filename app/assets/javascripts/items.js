9793dacf2edb56dfd7c260a7207b9f4d591c014f
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-14 08:34:26 +0900

    追加：items.jsのコメント
    コードの意図を表記するため、コメントを追加した。また、現段階で不要なコードを削除した。

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', ()=> {
 
-  // 画像用input要素
+  // 画像用input要素生成のための関数
   const buildFileField = (index) => {
     const html = `<div class="js-file_group" data-index="${index}">
                     <input class="js-file" type="file" name="item[images_attributes][${index}][src]" id="item_images_attributes_${index}_src">
@@ -8,7 +8,7 @@ $(document).on('turbolinks:load', ()=> {
     return html;
   }
 
-  // プレビュー
+  // プレビュー生成のための関数
   const buildImage = (index, url) => {
     const html = `<div class="preview">
                     <img data-index="${index}" src="${url}" width="120px" height="120px">
@@ -18,36 +18,44 @@ $(document).on('turbolinks:load', ()=> {
   }
 
   let count = 0;
+  //  画像用input要素に入れるindex番号
   let fileIndex = [1, 2, 3, 4];
 
+
+  // 画像用input要素に入力があったときの処理
   $('#image-box').on('change', '.js-file', function(e) {
     const targetIndex = $(this).parent().data('index');
+    // 画像URLの取得
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
 
-    if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
-      img.setAttribute('src', blobUrl);
-    } else {
-      count ++;
-      $('.dropbox__img').attr('for', `item_images_attributes_${count}_src`).before(buildImage(targetIndex, blobUrl));
-      if ($('.preview').length === 5) {
-        $('label').hide();
-      }
+    count ++;
+    // for属性を指定したlabelの追加
+    $('.dropbox__img').attr('for', `item_images_attributes_${count}_src`).before(buildImage(targetIndex, blobUrl));
+    // 画像が5枚になったらlabelの除去
+    if ($('.preview').length === 5) {
+      $('label').hide();
     }
-    // fileIndexの削除と追加
+    
+    // 画像用input要素に入れるindex番号の削除と追加
     $('#image-box').append(buildFileField(fileIndex[0]));
     fileIndex.shift();
     fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
   });
 
-  // 画像の要素とプレビューの削除
+  // 削除ボタンが押されたときの処理
   $(document).on('click', '.img-dele-btn', function() {
+    // 削除ボタンのidの取得
     let id = $(this).attr('id').slice(11);
+    // 削除ボタンが押された画像の、input要素部分の削除
     $(`#item_images_attributes_${id}_src`).parent().remove();
+    // プレビュー部分の削除
     $(this).parent().remove();
     
+    // 一度5枚分プレビューした画像が消された場合
     if ($('.preview').length < 5) {
       $('#image-box').append(buildFileField(fileIndex[0]));
+      // labelの再表示とindex番号の追加
       $('label').show();
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
     }  
