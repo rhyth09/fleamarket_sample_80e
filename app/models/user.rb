1aa4ee0de4265271eceb4c727ffc74d2a47c86de
ryokumagai <rk1996kumagai@gmail.com>
2020-08-14 09:49:31 +0900

    修正：Userモデルのアソシエーション
    Userモデルのアソシエーションについて、user情報が削除された場合に、それに紐づくAdress情報が削除されるように変更した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_year, :birth_month, :birth_day ,presence: true
-  has_one :address
+  has_one :address, dependent: :destroy
 end
