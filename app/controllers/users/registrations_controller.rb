67d299a1a0713ede98c23824410013c04070072f
ryokumagai <rk1996kumagai@gmail.com>
2020-08-14 09:23:48 +0900

    修正：registrations_controller
    registrations_controllerの記載に誤りがあっため、修正した。

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,11 +13,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     @user = User.new(sign_up_params)
     unless @user.valid?
-      flash.now[:aleat] = @user.errors.full_messages
+      flash.now[:alert] = @user.errors.full_messages
       render :new and return
     end
     session["devise.regist_data"] = {user: @user.attributes}
-    session["devise.regist_data"][:user]["password"] = params[:user]["password"]
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
     @address = @user.build_address
     render :new_address
   end
