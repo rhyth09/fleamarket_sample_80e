a9495ba85a741e89b07afc24232409c2594e1f09
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-15 10:24:01 +0900

    追加：商品販売状況のアソシエーション
    商品の販売状況をuserモデルから呼び出せるよう、app/models/user.rbにおいてアソシエーションを定義した。
    
    なお商品の販売状況は、Itemsテーブルにおけるseller_idとbuyer_idの情報に基づくものである。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :seller, class_name: "User"
+  belongs_to :seller, class_name: "User", foreign_key: "seller_id"
   belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_year, :birth_month, :birth_day ,presence: true
   has_one :address, dependent: :destroy
+  has_many :bought_items, foreign_key: "buyer_id", class_name: "Item"
+  has_many :sales_items, -> { where buyer_id: nil }, foreign_key: "seller_id", class_name: "Item"
+  has_many :sold_items, -> { where.not buyer_id: nil }, foreign_key: "seller_id", class_name: "Item"
 end
