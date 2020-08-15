49c5a550f9a8f46be08daf7ea041e7da803dd347
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-15 09:13:17 +0900

    追加：seller_idを含む商品の登録
    itemsテーブルに保存する際、ログイン中のユーザーのidをseller_idとしてパラメーターに含めるよう、mergeメソッドを用いて記述した。
    
    また、今回保存を妨げていたbuyerに関するアソシエーションを削除した。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :seller, class_name: "User"
-  belongs_to :buyer, class_name: "User"
   belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
