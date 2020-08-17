6ea2cec769ac3d089109c8e859bf1318c434c47a
ryokumagai <rk1996kumagai@gmail.com>
2020-08-17 18:33:31 +0900

    追加：都道府県の選択機能
    お届け先情報を登録する際に都道府県の項目をactive_hashから選択できるように設定した。

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,9 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
   validates :send_first_name, :send_last_name, :send_first_name_kana, :send_last_name_kana, :postal_code, :prefecture_id, :city, :address ,presence: true
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
 end
