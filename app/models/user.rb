0a367359a37fc4ddf3d6469899b290b6bd0bda6e
ryokumagai <rk1996kumagai@gmail.com>
2020-08-18 23:30:52 +0900

    修正：user.rbのインデント
    コンフリクト解消時にずれてしまったインデントを修正した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
   validates :password, length: { minimum: 7 }
   validates :last_name, :first_name,  presence: true,
-  format: {
-    with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
-    message: "は全角で入力して下さい"
-  }
+                                      format: {
+                                        with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
+                                        message: "は全角で入力して下さい"
+                                      }
   validates :last_name_kana, :first_name_kana,  presence: true,
-  format: {
-    with: /\A[ァ-ヶー－]+\z/,
-    message: "は全角カタカナで入力して下さい"
-  }
+                                                format: {
+                                                  with: /\A[ァ-ヶー－]+\z/,
+                                                  message: "は全角カタカナで入力して下さい"
+                                                }
   validates :birth_year, :birth_month, :birth_day, presence: true
 
 
