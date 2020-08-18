f79cde2aa682e2c0d02b4a2f7bea97622f3b271c
ryokumagai <rk1996kumagai@gmail.com>
2020-08-18 10:52:27 +0900

    追加：Addressモデルのバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,18 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
-  validates :send_first_name, :send_last_name, :send_first_name_kana, :send_last_name_kana, :postal_code, :prefecture_id, :city, :address ,presence: true
+
+  validates :send_last_name, :send_first_name,  presence: true,
+                                                format: {
+                                                with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
+                                                message: "は全角で入力して下さい"
+                                                }
+validates :send_last_name_kana, :send_first_name_kana,  presence: true,
+                                                        format: {
+                                                        with: /\A[ァ-ヶー－]+\z/,
+                                                        message: "は全角カタカナで入力して下さい"
+                                                        }
+
+validates :postal_code, :prefecture_id, :city, :address ,presence: true
 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
