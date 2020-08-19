1c236f17f3236159723bfdf85f32c7d96b513b92
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-19 17:48:29 +0900

    追加：画像保存の際のリサイズ
    保存の際の画像のリサイズ設定を追記した。

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fit: [120, 120]
+  process resize_to_fill: [720, 720, "center"]
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
