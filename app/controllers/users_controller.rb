ee4a52f82bfd42accf13130f7367d784acf361b1
n0808 <life.with.chums.119@gmail.com>
2020-08-19 20:45:00 +0900

    追加：before_action
    ユーザーコントローラーにbefore_action を設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show,]
+
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
     @items = user.items
   end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 
 end
