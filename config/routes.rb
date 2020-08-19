f4a290b0bb5e4e96a4561c236ada4faf20269e48
kayahikomakita <kmakitatech@gmail.com>
2020-08-19 14:43:01 +0900

    追加　修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
 
 
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  
 
-  # devise_for :users, controllers: {
-    # registrations: 'users/registrations',
-  # }
-  # devise_scope :user do
-    # get 'addresses', to: 'users/registrations#new_address'
-    # post 'addresses', to: 'users/registrations#create_address'
-  # end
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
 
   root 'items#index'
   resources :items, only: [:new, :create, :show] do
