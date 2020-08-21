446bcdf359d19c507719c151aafafeb5f045cef1
rhyth09 <kaleidostar0917@yahoo.co.jp>
2020-08-21 18:39:03 +0900

    修正：カテゴリ表示
    カテゴリ表示を新規カテゴリと同じにしました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_category, only: [:new, :edit, :create, :update, :destroy]
 
   def index
-    @items = Item.includes(:images, :user).order('created_at DESC')
+    @items = Item.includes(:images, :user).order('created_at DESC').limit(5)
   end
 
   def new
