3489ed4006bfa74bc3d5f39a60967d7923750d50
n0808 <life.with.chums.119@gmail.com>
2020-08-26 00:08:42 +0900

    削除：コメントマイグレーションファイルのuser削除
    user=sellerのため削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_092325) do
+ActiveRecord::Schema.define(version: 2020_08_25_122430) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -37,13 +37,12 @@ ActiveRecord::Schema.define(version: 2020_08_25_092325) do
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
     t.bigint "item_id"
     t.string "comment", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "seller_id", null: false
     t.index ["item_id"], name: "index_comments_on_item_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -94,7 +93,6 @@ ActiveRecord::Schema.define(version: 2020_08_25_092325) do
 
   add_foreign_key "addresses", "users"
   add_foreign_key "comments", "items"
-  add_foreign_key "comments", "users"
   add_foreign_key "images", "items"
   add_foreign_key "items", "categories"
 end
