69496dc23d99b0264d97247d2ef2de422c94b7cb
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-25 15:48:03 +0900

    追加：keydownにおける販売手数料および販売利益の表示
    キーボード操作で数値が入力された場合でも、販売手数料と販売価格が表示されるよう記述を追加した。

--- app/assets/javascripts/price.js
+++ app/assets/javascripts/price.js
@@ -9,4 +9,13 @@ $(document).on('turbolinks:load', ()=> {
     $('#hidden-price').prop('value', profit);
   });
 
+  $('.select--wrap').on('keydown', '#price', function() {
+    const fee = Math.floor(this.value * 0.1);
+    const profit = this.value - fee;
+    $('#fee').text(`¥ ${fee}`);
+    $('#profit').text(`¥ ${profit}`);
+
+    $('#hidden-price').prop('value', profit);
+  });
+
 });
