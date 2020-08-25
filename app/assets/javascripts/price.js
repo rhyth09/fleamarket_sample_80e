c9d0f21cb602c0d6e1ea6196c2040f6c2d0f1bc9
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-25 15:44:25 +0900

    追加：販売手数料と販売利益の計算
    販売価格から販売手数料と販売利益を計算し、テキストに表示させるための記述を追加した。

--- app/assets/javascripts/price.js
+++ app/assets/javascripts/price.js
@@ -1,3 +1,10 @@
 $(document).on('turbolinks:load', ()=> {
 
+  $('.select--wrap').on('change', '#price', function() {
+    const fee = Math.floor(this.value * 0.1);
+    const profit = this.value - fee;
+    $('#fee').text(`¥ ${fee}`);
+    $('#profit').text(`¥ ${profit}`);
+  });
+
 });
