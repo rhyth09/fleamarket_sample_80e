411ae03650a354a742dc041de6c50fdccd5a577c
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-25 13:04:31 +0900

    追加：/usersのルーティング
    /usersをusers/registrations#newへのルーティングとして設定した。

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
+    get 'users', to: 'users/registrations#new'
   end
 
   root 'items#index'
