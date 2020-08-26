e7fce38387408f8891538057158177f8a1f83097
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-26 13:10:02 +0900

    修正：イベント重複部分
    重複していた記述を一つにまとめた。

--- app/assets/javascripts/price.js
+++ app/assets/javascripts/price.js
@@ -1,15 +1,6 @@
 $(document).on('turbolinks:load', ()=> {
 
-  $('.select--wrap').on('change', '#price', function() {
-    const fee = Math.floor(this.value * 0.1);
-    const profit = this.value - fee;
-    $('#fee').text(`¥ ${fee}`);
-    $('#profit').text(`¥ ${profit}`);
-
-    $('#hidden-price').prop('value', profit);
-  });
-
-  $('.select--wrap').on('keydown', '#price', function() {
+  $('.select--wrap').on('change keydown', '#price', function() {
     const fee = Math.floor(this.value * 0.1);
     const profit = this.value - fee;
     $('#fee').text(`¥ ${fee}`);
