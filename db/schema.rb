ee8e22a38a757c5bd92d8bd46b665a502fa75ccf
ryokumagai <rk1996kumagai@gmail.com>
2020-08-26 15:13:39 +0900

    修正：itemモデルのアソシエーション
    itemモデルのアソシエーションを修正した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_08_25_122430) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -102,8 +101,8 @@ ActiveRecord::Schema.define(version: 2020_08_25_122430) do
   end
 
   add_foreign_key "addresses", "users"
-  add_foreign_key "comments", "items"
   add_foreign_key "cards", "users"
+  add_foreign_key "comments", "items"
   add_foreign_key "images", "items"
   add_foreign_key "items", "categories"
 end
