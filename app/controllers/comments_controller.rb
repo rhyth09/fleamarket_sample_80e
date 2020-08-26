cfaee7007321f6b598a57c0e8aae691d579f5fc6
n0808 <life.with.chums.119@gmail.com>
2020-08-26 22:10:43 +0900

    追加：json形式でのデータを送信する設定
    json形式で通信ができなかった場合はhtmlでリダイレクトする設定を行った

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,16 @@
 class CommentsController < ApplicationController
 
+  # def index
+  #   @comment = Comment.new
+  #   @comments = Comment.all
+  # end
+
   def create
     @item = Item.find(params[:item_id])
-    @comment = Comment.new(comment_params)
-    if @comment.save
-      redirect_to item_path(@comment.item.id)
-    else
-      render :show
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.html  { redirect_to item_path(params[:item_id]) }
+      format.json
     end
   end
 
---
+++ app/views/comments/create.json.jbuilder
@@ -0,0 +1,7 @@
+json.comment_item_id      @comment.item_id
+json.comment         @comment.comment
+json.comment_seller_id @comment.seller_id
+json.name            @comment.seller.nickname
+json.comment_item_id @comment.item_id
+json.item_id         @item.id
+json.item_seller_id    @item.seller_id
