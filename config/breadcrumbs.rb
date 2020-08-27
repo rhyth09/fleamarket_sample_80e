7fc3eb8b841df5a188a2918f2f86913e3da27aa6
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-27 11:22:16 +0900

    追加：マイページのサイドバーから飛べるページでのパンくず
    サイドバーにリンクが設定してあるページへのパンくずルートを追加した。

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,6 +2,22 @@ crumb :root do
   link "トップページ", root_path
 end
 
+# マイページから
 crumb :mypage do
   link "マイページ", user_path
+end
+
+crumb :sales_items do
+  link "出品中の商品", sales_items_user_path(current_user.id)
+  parent :mypage
+end
+
+crumb :sold_items do
+  link "売却済みの商品", sold_items_user_path(current_user.id)
+  parent :mypage
+end
+
+crumb :logout do
+  link "ログアウト", logout_user_path(current_user.id)
+  parent :mypage
 end
