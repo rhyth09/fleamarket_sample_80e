958234f3fd7b3d51d1a7e6af7fdb27d353c9be85
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-27 12:43:44 +0900

    追加：売却済み商品ページへのルーティング
    売却済み商品ページへのルーティングを設定した。

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     member do
       get 'sales_items'
+      get 'sold_items'
       get 'logout'
     end
     collection do
