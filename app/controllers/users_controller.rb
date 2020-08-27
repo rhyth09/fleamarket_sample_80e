1cbe0d0cf805f977e26ee2530940e93c8e7eb994
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-08-27 10:24:08 +0900

    追加：sold_itemsアクションとインスタンス変数の定義
    usersコントローラーにおいて、sold_itemsアクションを追加した。
    
    また、ビューで使用するインスタンス変数の定義をした。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
   def sales_items
     @sales_items = User.find(params[:id]).sales_items.includes(:images)
   end
+
+  def sold_items
+    @sold_items = User.find(params[:id]).sold_items.includes(:images)
+  end
   
   def logout
   end
